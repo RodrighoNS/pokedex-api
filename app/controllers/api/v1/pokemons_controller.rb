051c00a8463cd16cfd299a6f942e76647c6b05c5
Rodrigo Navarro <rodrigo@garagelabs.cl>
2023-11-07 14:27:05 -0300

    Add Api versioning

---
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -0,0 +1,57 @@
+module Api
+  module V1
+    class PokemonsController < ApplicationController
+      include Pagy::Backend
+      before_action :set_pokemon, only: %i[ show update destroy ]
+
+      # GET /pokemons
+      def index
+        @pagy, @pokemons = pagy(Pokemon.all)
+        pagy_headers_merge(@pagy)
+        render jsonapi: @pokemons
+      end
+
+      # GET /pokemons/1
+      def show
+        render jsonapi: @pokemon
+      end
+
+      # POST /pokemons
+      def create
+        @pokemon = Pokemon.new(pokemon_params)
+
+        if @pokemon.save
+          render jsonapi: @pokemon, status: :created, location: @pokemon
+        else
+          render jsonapi: @pokemon.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /pokemons/1
+      def update
+        if @pokemon.update(pokemon_params)
+          render jsonapi: @pokemon
+        else
+          render jsonapi: @pokemon.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /pokemons/1
+      def destroy
+        @pokemon.destroy!
+        render json: { message: "Pokemon deleted" }
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_pokemon
+          @pokemon = Pokemon.find_by(pokemon_id: params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def pokemon_params
+          params.fetch(:pokemon, {}).permit!
+        end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :pokemons
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
+  namespace :api do
+    namespace :v1 do
+      resources :pokemons
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
